7bf9259ecf428e894cafc7033a89954fdd2aa56b
Jason <jasonlikenfs@gmail.com>
2016-08-08 11:52:40 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,19 @@
-var client = require('wechat-jssdk/client');
+var should = require('chai').should();
+var wx = require('../lib');
 
-console.log(client);
+wx.initialize({
+  "wechatToken": "6mwdIm9p@Wg7$Oup",
+  "appId": "wxfc9c5237ebf480aa",
+  "appSecret": "2038576336804a90992b8dbe46cd5948"
+});
+
+describe('jssdk', function() {
+  describe('#getAccessToken()', function() {
+    it('should get wechat token', function(done) {
+      wx.jssdk.getAccessToken(function (data) {
+        data.should.have.property('access_token');
+        done();
+      });
+    });
+  });
+});
