8cfe765ae33743bae44f47f7d6ad8b05148908df
Jason <jasonlikenfs@gmail.com>
2016-11-09 18:55:39 +0800

    refact client side js, update ver to 2.1.0

--- client.js
+++ client.js
@@ -1,162 +1,198 @@
-/**
+/*!
  * @license MIT
- * client side js to use wechat jssdk,
+ * Client side js to use wechat-jssdk, also works with other server side service.
  * https://github.com/JasonBoy/wechat-jssdk
  */
 
-;!(function(window, document, location) {
-  var _script_url = location.protocol + '//res.wx.qq.com/open/js/jweixin-1.0.0.js';
-  var defaultApiList = ['onMenuShareTimeline', 'onMenuShareAppMessage'];
-  /**
-   *
-   * @param wechatConfig, should contain like:
-   *   {
-   *      appId: 'xxxx',
-   *      timestamp: '',
-   *      nonceStr: '',
-   *      signature: '',
-   *      jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage']
-   *   }
-   * @param cbSuccess {function}
-   * @param cbError {function}
-   * @param debug {boolean}
-   * @param customUrl
-   * @returns {WechatJSSDK}
-   */
-  function WechatJSSDK (wechatConfig, cbSuccess, cbError, debug, customUrl) {
-    customUrl && (_script_url = customUrl);
-    //using new _wechat_jssdk(config);
-    if(this instanceof WechatJSSDK) {
-      this.config = wechatConfig;
-      var apiList = this.config.jsApiList;
-      if(!apiList || apiList.length <= 0) {
-        this.config.jsApiList = defaultApiList;
-      } else {
-        for(var i = 0, length = defaultApiList.length; i < length; i++) {
-          var defaultItem = defaultApiList[i];
-          if(apiList.indexOf(defaultItem) < 0) {
-            apiList.push(defaultItem);
-          }
-        }
-      }
-      this.debug = wechatConfig.hasOwnProperty('debug') ? wechatConfig.debug : true === debug;
-      if(wechatConfig.success) {
-        this.cbSuccess = wechatConfig.success;
-        delete wechatConfig.success;
-      } else {
-        this.cbSuccess = cbSuccess;
-      }
-      if(wechatConfig.error) {
-        this.cbError = wechatConfig.error;
-        delete wechatConfig.error;
-      } else {
-        this.cbError = cbError;
-      }
-      this.loadScript();
-    } else {
-      return new WechatJSSDK(wechatConfig, cbSuccess, cbError, debug, customUrl);
-    }
-  }
+;!(function (window, document, location) {
+  'use strict';
 
-  var _moment_config = {
+  //default wechat script url
+  var defaultScriptUrl = location.protocol + '//res.wx.qq.com/open/js/jweixin-1.0.0.js';
+
+  //default apis with share-on-moment and share-on-chat
+  var defaultApiList = ['onMenuShareTimeline', 'onMenuShareAppMessage'];
+
+  //set some default share config
+  var defaultMomentConfig = {
     title: document.title,
     link: location.href,
     type: 'link'
-    // imgUrl: location.origin + '/public/img/logo.png'
   };
-  var _chat_config = {
+  var defaultChatConfig = {
     title: document.title,
     link: location.href,
     type: 'link',
     desc: document.title
   };
 
-  WechatJSSDK.prototype.signSignature = function() {
-    var self = this;
-    var config = this.config;
-    var debug = this.debug;
-    if(debug) {
-      config.debug = debug;
+  //sdk prototype
+  var sdk = WechatJSSDK.prototype;
+
+  /**
+   * Initialize the WechatJSSDK instance
+   * @param {object} wechatConfig, should contain like:
+   *   {
+   *      appId: 'xxxx',
+   *      timestamp: '',
+   *      nonceStr: '',
+   *      signature: '',
+   *      jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', ...],
+   *      success: function(){}, //sign success callback
+   *      error: function(){}, //sign error callback
+   *      customUrl: 'http://res.wx.qq.com/open/js/jweixin-1.0.0.js' // set custom weixin script url
+   *   }
+   * @returns {WechatJSSDK}
+   */
+  function WechatJSSDK(wechatConfig) {
+    //using new _wechat_jssdk(config);
+    if (this instanceof WechatJSSDK) {
+      this.config = wechatConfig || {};
+      if (this.config.customUrl) {
+        defaultScriptUrl = this.config.customUrl;
+        delete this.config.customUrl;
+      }
+      var apiList = this.config.jsApiList;
+      //add more apis if passed in
+      if (!apiList || apiList.length <= 0) {
+        this.config.jsApiList = defaultApiList;
+      } else {
+        for (var i = 0, length = defaultApiList.length; i < length; i++) {
+          var defaultItem = defaultApiList[i];
+          if (apiList.indexOf(defaultItem) < 0) {
+            apiList.push(defaultItem);
+          }
+        }
+      }
+      this.debug = !!this.config.debug;
+      this.loadScript();
+      return this;
     }
-    if(!window.wx) {
+    return new WechatJSSDK(wechatConfig);
+  }
+
+  /**
+   * Sign the signature now
+   * @param {object} newSignConfig, debug mode, appId, jsApiList cannot be changed!!!
+   *        , should only provide new signature specific config
+   * @returns {WechatJSSDK} sdk instance
+   */
+  sdk.signSignature = function (newSignConfig) {
+    var self = this;
+    var selfConfig = self.config;
+    var config = newSignConfig || selfConfig;
+    var signConfig = {
+      debug: self.debug,
+      appId: selfConfig.appId,
+      timestamp: config.timestamp || selfConfig.timestamp,
+      nonceStr: config.nonceStr || selfConfig.nonceStr,
+      signature: config.signature || selfConfig.signature,
+      jsApiList: selfConfig.jsApiList
+    };
+    var debug = self.debug;
+    if (!window.wx) {
       console.warn('wechat js not defined');
-      return;
+      return this;
     }
     var wx = window.wx;
-    wx.config(config);
+    wx.config(signConfig);
     wx.ready(function () {
       console.log('sign signature finished...');
       debug && alert('sign signature finished...');
       //initialize share on moment and chat features
-      wx['onMenuShareTimeline'](_moment_config);
-      wx['onMenuShareAppMessage'](_chat_config);
+      wx['onMenuShareTimeline'](defaultMomentConfig);
+      wx['onMenuShareAppMessage'](defaultChatConfig);
       self.signFinished = true;
-      self.cbSuccess && self.cbSuccess();
+      config.success && config.success.call(self);
     });
 
     wx.error(function (err) {
-      debug && alert(err ? ('sign err: ' + JSON.stringify(err)) : 'ok');
-      self.cbError && self.cbError(err);
+      debug && alert('sign error: ' + JSON.stringify(err));
+      config.error && config.error.call(self, err);
     });
 
     //export original wx object
     self.wx || (self.wx = wx);
-  };
-  
-  WechatJSSDK.prototype.updateConfig = function(newConfig) {
-    delete newConfig.success;
-    delete newConfig.error;
-    this.config = newConfig;
+    return self;
   };
 
-  WechatJSSDK.prototype.loadScript = function() {
+  /**
+   * Load wechat js script and sign the signature
+   * @returns {WechatJSSDK}
+   */
+  sdk.loadScript = function () {
     var self = this;
     var ele = document.createElement('script');
-    ele.src = _script_url;
+    ele.src = defaultScriptUrl;
     ele.async = true;
     var linkEle = document.getElementsByTagName('script')[0];
     linkEle.parentNode.insertBefore(ele, linkEle);
-    ele.onload = function() {
-      console.log('script loaded');
+    ele.onload = function () {
+      console.log('Wechat script loaded successfully!');
       //init the wechat config
       self.signSignature();
     };
+    ele.onerror = function (err) {
+      console.error('Failed to load wechat script!');
+      console.error(err);
+      self.debug && alert('Cannot load wechat script!');
+    };
+    return self;
   };
 
-  WechatJSSDK.prototype.setMomentConfig = function(info) {
-    if(!info) return;
-    for(var key in info) {
-      if(info.hasOwnProperty(key)) {
-        _moment_config[key] = info[key];
+  /**
+   * quick way to set custom moment share configs
+   * @param {object} info
+   * @returns {WechatJSSDK}
+   */
+  sdk.setMomentConfig = function (info) {
+    if (!info) return this;
+    for (var key in info) {
+      if (info.hasOwnProperty(key)) {
+        defaultMomentConfig[key] = info[key];
       }
     }
+    return this;
   };
 
-  WechatJSSDK.prototype.setChatConfig = function(info) {
-    if(!info) return;
-    for(var key in info) {
-      if(info.hasOwnProperty(key)) {
-        _chat_config[key] = info[key];
+  /**
+   * Quick way to set custom chat share configs
+   * @param {object} info
+   * @returns {WechatJSSDK}
+   */
+  sdk.setChatConfig = function (info) {
+    if (!info) return this;
+    for (var key in info) {
+      if (info.hasOwnProperty(key)) {
+        defaultChatConfig[key] = info[key];
       }
     }
+    return this;
   };
 
-  //call other wechat api manually
-  WechatJSSDK.prototype.callWechatApi = function(apiName, config) {
-    if(!apiName) return;
-    if(this.config.jsApiList.indexOf(apiName) < 0) {
+  /**
+   * Call any wechat api
+   * @param {string} apiName
+   * @param {object} config specific api config
+   * @returns {WechatJSSDK}
+   */
+  sdk.callWechatApi = function (apiName, config) {
+    if (!apiName) return this;
+    var debug = this.debug;
+    if (this.config.jsApiList.indexOf(apiName) < 0) {
       debug && alert('the wechat api [' + apiName + '] you call was not registered, \npls add the api into your [jsApiList] config');
-      return;
+      return this;
     }
-    var customAPI = window.wx[apiName];
-    if(!customAPI || 'function' !== typeof customAPI) {
+    var customAPI = this.wx[apiName];
+    if (!customAPI || 'function' !== typeof customAPI) {
       debug && alert('no such api [' + apiName + '] found!');
-      return;
+      return this;
     }
     customAPI(config);
+    return this;
   };
 
-  if('undefined' !== typeof module && 'undefined' !== typeof exports) {
+  if ('undefined' !== typeof module && 'undefined' !== typeof exports) {
     module.exports = WechatJSSDK;
   } else {
     window._wechat_jssdk = WechatJSSDK;
