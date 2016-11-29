06b902fa527d6467f5d32cbd1e419718bf39fef4
Jason <jasonlikenfs@gmail.com>
2016-11-29 16:05:11 +0800

    convert syntax to ES6

--- demo/index.js
+++ demo/index.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var http = require("http");
-var swig = require('swig');
-var wx = require('../lib');
-var path = require("path");
+const express = require('express');
+const http = require("http");
+const swig = require('swig');
+const wx = require('../lib');
+const path = require("path");
 wx.initialize({
   "wechatToken": "6mwdIm9p@Wg7$Oup",
   "appId": "wxfc9c5237ebf480aa",
-  "appSecret": "2038576336804a90992b8dbe46cd5948"
+  "appSecret": "2038576336804a90992b8dbe46cd5948",
 });
 
-var app = express();
+const app = express();
 swig.setDefaults({
-  cache: false
+  cache: false,
 });
 
 app.engine('html', swig.renderFile);
@@ -32,10 +32,10 @@ app.get('/api/wechat', function (req, res) {
 
 app.get('/get-signature', function(req, res) {
   console.log(req.query);
-  wx.jssdk.getSignatureByURL(req.query.url).then(function(data) {
+  wx.jssdk.getSignatureByURL(req.query.url).then((data) => {
     console.log('OK', data);
     res.json(data);
-  }, function(reason) {
+  }, (reason) => {
     console.error(reason);
     res.json(reason);
   });
@@ -45,6 +45,6 @@ app.get('/client.js', function (req, res) {
   res.sendFile(path.join(__dirname, '../client.js'));
 });
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 //should use like nginx to proxy to the request to 3000, the signature domain must be on 80 PORT.
 server.listen(3000);
