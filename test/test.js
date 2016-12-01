cee8198ee8edcaac2082c1e2c97b524efa7fc0be
Jason <jasonlikenfs@gmail.com>
2016-12-01 14:52:09 +0800

    fix test usage

--- test/test.js
+++ test/test.js
@@ -1,36 +1,36 @@
 let should = require('chai').should();
-const wx = require('../lib');
+const Wechat = require('../lib');
 const config = {
   "wechatToken": "6mwdIm9p@Wg7$Oup",
   "appId": "wxfc9c5237ebf480aa",
   "appSecret": "2038576336804a90992b8dbe46cd5948",
 };
 
-wx.initialize(config);
+const wx = new Wechat(config);
 
-describe('jssdk', function() {
-  describe('#getAccessToken()', function() {
-    it('should get wechat token', function(done) {
+describe('jssdk', function () {
+  describe('#getAccessToken()', function () {
+    it('should get wechat token', function (done) {
       this.timeout(20000);
-      wx.jssdk.getAccessToken().then(function(data) {
+      wx.jssdk.getAccessToken().then(function (data) {
         data.should.have.property('access_token');
         done();
       });
     });
   });
-  describe('#getSignatureByURL()', function() {
-    it('should get signature', function(done) {
+  describe('#getSignatureByURL()', function () {
+    it('should get signature', function (done) {
       this.timeout(20000);
       const url = 'http://localhost?test_signature';
       wx.jssdk.getSignatureByURL(url)
-          .then(function (signature) {
-            signature.should.be.an('object');
-            signature.should.have.property('url').equal(url);
-            signature.should.have.property('nonceStr');
-            signature.should.have.property('signature');
-            signature.should.have.property('timestamp');
-            done();
-          });
+        .then(function (signature) {
+          signature.should.be.an('object');
+          signature.should.have.property('url').equal(url);
+          signature.should.have.property('nonceStr');
+          signature.should.have.property('signature');
+          signature.should.have.property('timestamp');
+          done();
+        });
     });
   });
 });
