b5ed0a6789436063620749aaede1a94ddfb3cce5
Jason <jasonlikenfs@gmail.com>
2016-12-22 14:58:13 +0800

    use FileStore for demo

--- demo/index.js
+++ demo/index.js
@@ -16,8 +16,8 @@ const wx = new Wechat({
   "appId": "wxfc9c5237ebf480aa",
   "appSecret": "2038576336804a90992b8dbe46cd5948",
   "wechatRedirectUrl": "http://127.0.0.1/oauth",
-  store: new MongoStore({limit: 5}),
-  // store: new FileStore({interval: 1000 * 60 * 3}),
+  // store: new MongoStore({limit: 5}),
+  store: new FileStore({interval: 1000 * 60 * 3}),
 });
 
 const app = express();
