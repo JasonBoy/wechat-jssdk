ed6293ce016b67ac8d844bcf4a7b985aeff8cd34
jason <jasonlikenfs@gmail.com>
2023-04-28 18:13:19 +0800

    feat: regen web app

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -33,7 +33,6 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="text-center">wechat-jssdk</div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -44,7 +43,7 @@ export default function Home() {
           priority
         />
       </div>
-
+      <div className="text-center">wechat-jssdk v7</div>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
